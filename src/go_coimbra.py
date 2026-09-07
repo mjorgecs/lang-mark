c82ceef9d4f614ee422354b0ffa0aa57a4d867a1
GoodBarber <goodbarber@Mac-mini-de-GoodBarber.local>
2026-09-07 15:50:09 +0100

    create routes

--- src/go_coimbra.py
+++ src/go_coimbra.py
@@ -1,20 +1,33 @@
 import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_chroma import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 
-PDF_PATH = "../docs/do_coimbra/700-maiores-empresas-coimbra-2025-extended.pdf"
+from typing import List
+from typing_extensions import TypedDict
 
+
+PDF_PATH = "../docs/do_coimbra/700-maiores-empresas-coimbra-2025-extended.pdf"
 COLLECTIONS_PATH = "../collections"
+
+### SETUP
 
 # Load .env info
 load_dotenv()
 api_key = os.getenv('GOOGLE_API_KEY')
 
 
-# Set the AI embedding model up
-llm = GoogleGenerativeAIEmbeddings(
+# Set up the AI embedding model
+embeddings = GoogleGenerativeAIEmbeddings(
+  model= "gemini-3.5-flash",
+  temperature = 0,
+  max_retries = 2,
+  google_api_key = api_key,
+)
+
+llm = ChatGoogleGenerativeAI(
   model= "gemini-3.5-flash",
   temperature = 0,
   max_retries = 2,
@@ -22,12 +35,14 @@ llm = GoogleGenerativeAIEmbeddings(
 )
 
 
-# Load the PDF to index into the Chroma DB
+### CREATE INDEX
 
+# Load the PDF to index into the ChromaDB
 if not os.path.exists(PDF_PATH):
   raise FileNotFoundError(f"PDF file not found: {PDF_PATH}")
 
 pdf_loader = PyPDFLoader(PDF_PATH) # This loads the PDF
+
 
 # Checks if the PDF is there
 try:
@@ -43,9 +58,95 @@ text_splitter = RecursiveCharacterTextSplitter(
   chunk_overlap=200
 )
 
+
 # Split the pages into chunks
 pages_split = text_splitter.split_documents(pages)
 
-collection_name = "700-extended"
-if not os.path.exists(COLLECTIONS_PATH):
-  raise FileNotFoundError(F"Directory not found: {COLLECTIONS_PATH}")
+
+# Create the ChromaDB
+try:
+
+  collection_name = "700-extended"
+  if not os.path.exists(COLLECTIONS_PATH):
+    raise FileNotFoundError(F"Directory not found: {COLLECTIONS_PATH}")
+
+  # Here, we actually create the chroma database using our embeddigns model
+  vectorstore = Chroma.from_documents(
+    documents=pages_split,
+    embedding=embeddings,
+    persist_directory=COLLECTIONS_PATH,
+    collection_name=collection_name
+  )
+  print(f"Created ChromaDB vector store!")
+
+except Exception as e:
+  print(f"Error setting up ChromaDB: {str(e)}")
+  raise
+
+
+# Create the retriever
+retriever = vectorstore.as_retriever(
+  search_type="similarity", # 'similarity' (default), 'mmr', or 'similarity_score_threshold'
+  search_kwargs={"k": 5} # K is the amount of chunks to return
+)
+
+
+
+
+
+### LLMs-------------------------------------------
+# Data model
+class GraphState(TypedDict):
+  """
+  Represents the state of our graph.
+
+  Attributes:
+    question: question
+    generation: LLM generation
+    documents: list of documents
+  """
+
+  question: str
+  generation: str
+  documents: List[str]
+
+
+# Nodes-------------------------------------------
+def retrieve(state):
+  """
+  Retrieve documents
+
+  Args:
+    state (dict): The current graph state
+
+  Returns:
+    state (dict): New key added to state, documents, that contains retrieved documents
+  """
+
+  print("--RETRIEVE--")
+  question = state["question"]
+
+  # Retrieval
+  documents = retriever.invoke(question)
+  return {"documents": documents, "question": question}
+
+
+def grade_documents(state):
+  """
+  Determines wether the retrieved documents are relevant to the question.
+
+  Args:
+    state (dict): The current graph state
+
+  Returns:
+    state (dict): Updates documents key with only filtered relevant documents
+  """
+
+  print("---CHECK DUCUMENT RELEVANCE TO QUESTION---")
+  question = state["question"]
+  documents = state["documents"]
+
+  # Score each document
+  filtered_docs = []
+  for d in documents:
+    score = retrive
---
+++ src/routes.py
@@ -0,0 +1,112 @@
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+### Retrieval Grader-------------------------------------------
+# Data model
+class GradeDocuments(BaseModel):
+  """Binary score for relevance check on retrieved documents."""
+
+  binary_score: str = Field(
+    description="Documents are relevant to the question, 'yes' or 'no'"
+  )
+
+def retrievel_grader(llm):
+  structured_llm_grader = llm.with_structured_output(GradeDocuments)
+
+  # Prompt
+  system = """You are a grader assessing relevance of a retrieved document to a user question. \n
+      If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
+      It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
+      Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
+
+  grade_prompt = ChatPromptTemplate.from_messages(
+    [
+      ("system", system),
+      ("human", "Retrieved document: \n\n {document} \n\n User question: {question}"),
+    ]
+  )
+
+  return grade_prompt | structured_llm_grader
+
+
+### Generate-------------------------------------------
+# Prompt
+def rag_chain(llm):
+  prompt = hub.pull("rlm/rag-prompt")
+
+  # Chain
+  return prompt | llm | StrOutputParser()
+
+
+### Hallucination Grader-------------------------------------------
+# Data model
+class GradeHallucinations(BaseModel):
+  """Binary score for hallucination present in generation answer."""
+
+  binary_score: str = Field(
+    description="Answer is grounded in the facts, 'yes' or 'no'"
+  )
+
+def hallucination_grader(llm):
+  structured_llm_grader = llm.with_structured_output(GradeHallucinations)
+
+  # Prompt
+  system = """You are a grader assessing whether an LLM generation is grounded in / supported by a set of retrieved facts. \n
+      Give a binary score 'yes' or 'no'. 'Yes' means that the answer is grounded in / supported by the set of facts."""
+
+  hallucination_prompt = ChatPromptTemplate.from_messages(
+    [
+      ("system", system),
+      ("human", "Set of facts: \n\n {documents} \n\n LLM generation: {generation}"),
+    ]
+  )
+
+  return hallucination_prompt | structured_llm_grader
+
+
+### Answer Grader-------------------------------------------
+# Data model
+class GradeAnswer(BaseModel):
+  """Binary score to assess answer addresses question."""
+
+  binary_score: str = Field(
+    description="Answer addresses the question, 'yes' or 'no'"
+  )
+
+def answer_grader(llm):
+  structured_llm_grader = llm.with_structured_output(GradeAnswer)
+
+  # Prompt
+  system = """You are a grader assessing whether an answer addresses / resolves a question \n
+      Give a binary score 'yes' or 'no'. Yes' means that the answer resolves the question."""
+
+  answer_prompt = ChatPromptTemplate.from_messages(
+    [
+      ("system", system),
+      ("human", "User question: \n\n {question} \n\n LLM generation: {generation}"),
+    ]
+  )
+
+  return answer_prompt | structured_llm_grader
+
+
+### Question Re-writer-------------------------------------------
+def question_rewriter(llm):
+  # Prompt
+  system = """You a question re-writer that converts an input question to a better version that is optimized \n
+      for vectorstore retrieval. Look at the input and try to reason about the underlying semantic intent / meaning."""
+
+  re_write_prompt = ChatPromptTemplate.from_messages(
+    [
+      ("system", system),
+      (
+        "human",
+        "Here is the initial question: \n\n {question} \n Formulate an improved question.",
+      ),
+    ]
+  )
+
+  return re_write_prompt | llm | StrOutputParser()
